c796f2ffe6215a32981bf716f96923b795b0cc81
Kriti2004 <kritikiran20@gmail.com>
2023-06-21 15:02:01 +0530

    Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// 1. Function Defination
+function getMyFullName(...args){ // Rest Operator
+    console.log(args);
+}
+
+// 2. function calling
+getMyFullName('Shruti',"Suman",30,5.6,["Ruhi", "Abhir"]);
+
---
+++ js/script2.js
@@ -0,0 +1,7 @@
+var x = ["Kriti"];
+var y = [...x, "Kiran"];
+var z = [...y, "Jha"]
+
+console.log(x);
+console.log(y);
+console.log(z);
